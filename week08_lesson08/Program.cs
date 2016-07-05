4d6500ca651d383925c3c22d81ce07660aa522ff
kferna34 <kferna34@my.centennialcollege.ca>
2016-07-05 13:01:20 -0400

    Add project files.

---
+++ week08_lesson08/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week08_lesson08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
